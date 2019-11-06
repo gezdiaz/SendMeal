a63aace9edf89c942565ecfa023f59aacf018e22
Tomas Ravelli <travelli@frsf.utn.edu.ar>
2019-11-06 12:15:07 -0300

    Actividad del mapa lab 5 terminada
    Falta detalles del spinner

--- app/src/main/java/frsf/isi/dam/gtm/sendmeal/domain/EstadoPedido.java
+++ app/src/main/java/frsf/isi/dam/gtm/sendmeal/domain/EstadoPedido.java
@@ -3,8 +3,8 @@ package frsf.isi.dam.gtm.sendmeal.domain;
 public enum EstadoPedido {
     PENDIENTE, ENVIADO, ACEPTADO, RECHAZADO, EN_PREPARACION, EN_ENVIO, ENTREGADO, CANCELADO;
 
-    public static EstadoPedido[] getSpinnerList(){
-        return new EstadoPedido[]{ENVIADO, ACEPTADO, RECHAZADO, EN_PREPARACION, EN_ENVIO, ENTREGADO};
+    public static String[] getSpinnerList(){
+        return new String[]{"Seleccione un estado...", ENVIADO.name(), ACEPTADO.name(), RECHAZADO.name(), EN_PREPARACION.name(), EN_ENVIO.name(), ENTREGADO.name()};
     }
 
 }
